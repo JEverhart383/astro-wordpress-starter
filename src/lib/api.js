9b68542f37472e9dc28e503b2c52ec979a82c3de
Jeff Everhart <Jeffeverhart383@gmail.com>
2022-11-04 10:44:05 -0400

    initial commit

---
+++ src/lib/api.js
@@ -0,0 +1,121 @@
+export async function navQuery(){
+    const siteNavQueryRes = await fetch(import.meta.env.WORDPRESS_API_URL, {
+        method: 'post', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            query: `{
+                menu(id: "MainMenu", idType: NAME) {
+                    name
+                    menuItems {
+                        nodes {
+                            uri
+                            url
+                            order
+                            label
+                        }
+                    }
+                }
+                generalSettings {
+                    title
+                    url
+                    description
+                }
+            }
+            `
+        })
+    });
+    const{ data } = await siteNavQueryRes.json();
+    return data;
+}
+
+export async function homePagePostsQuery(){
+    const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
+        method: 'post', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            query: `{
+                posts {
+                  nodes {
+                    date
+                    uri
+                    title
+                    commentCount
+                    excerpt
+                    categories {
+                      nodes {
+                        name
+                        uri
+                      }
+                    }
+                  }
+                }
+              }
+            `
+        })
+    });
+    const{ data } = await response.json();
+    return data;
+}
+
+
+export async function getNodeByURI(uri){
+    const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
+        method: 'post', 
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            query: `query MyQuery3($uri: String!) {
+                nodeByUri(uri: $uri) {
+                  __typename
+                  isContentNode
+                  isTermNode
+                  ... on Post {
+                    id
+                    title
+                    date
+                    uri
+                    excerpt
+                    content
+                    featuredImage {
+                      node {
+                        mediaItemUrl
+                        altText
+                      }
+                    }
+                  }
+                  ... on Page {
+                    id
+                    title
+                    uri
+                    date
+                    content
+                  }
+                  ... on Category {
+                    id
+                    name
+                    posts {
+                      nodes {
+                        date
+                        title
+                        excerpt
+                        uri
+                        featuredImage {
+                          node {
+                            altText
+                            mediaItemUrl
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            variables: {
+                uri: uri
+            }
+        })
+    });
+    const{ data } = await response.json();
+    return data;
+}
+
