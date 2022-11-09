fd303f0c5339dda1085a899d7b452f09d11b57d1
Jeff Everhart <Jeffeverhart383@gmail.com>
2022-11-09 11:59:04 -0500

    trim uri, change render methodc

--- src/lib/api.js
+++ src/lib/api.js
@@ -137,3 +137,45 @@ export async function getNodeByURI(uri){
     return data;
 }
 
+export async function getAllUris(){
+  const response = await fetch(import.meta.env.WORDPRESS_API_URL, {
+      method: 'post', 
+      headers: {'Content-Type':'application/json'},
+      body: JSON.stringify({
+          query: `query GetAllUris {
+            terms {
+              nodes {
+                uri
+              }
+            }
+            posts(first: 100) {
+              nodes {
+                uri
+              }
+            }
+            pages(first: 100) {
+              nodes {
+                uri
+              }
+            }
+          }
+          `
+      })
+  });
+  const{ data } = await response.json();
+  const uris = Object.values(data)
+    .reduce(function(acc, currentValue){
+      return acc.concat(currentValue.nodes)
+    }, [])
+    .map(node => {
+      let trimmedURI = node.uri.substring(1);
+      trimmedURI = trimmedURI.substring(0, trimmedURI.length - 1)
+      return {params: {
+        uri: trimmedURI
+      }}
+    })
+    console.log(uris)
+  return uris;
+
+}
+
